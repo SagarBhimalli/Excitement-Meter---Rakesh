903e7664494c067965a92b15dd8a1683a09ebf7a
SagarChoru56 <41792615+SagarChoru56@users.noreply.github.com>
2018-07-27 19:07:42 +0530

    Excitement Meter

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblConnectionId = new System.Windows.Forms.Label();
+            this.lblStatusValue = new System.Windows.Forms.Label();
+            this.lblConnectionIdValue = new System.Windows.Forms.Label();
+            this.lblPosition = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnStream = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.cboComPort = new System.Windows.Forms.ComboBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Snow;
+            this.lblStatus.Location = new System.Drawing.Point(16, 7);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(191, 29);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Kinect Status  : ";
+            // 
+            // lblConnectionId
+            // 
+            this.lblConnectionId.AutoSize = true;
+            this.lblConnectionId.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConnectionId.Location = new System.Drawing.Point(6, 42);
+            this.lblConnectionId.Name = "lblConnectionId";
+            this.lblConnectionId.Size = new System.Drawing.Size(189, 29);
+            this.lblConnectionId.TabIndex = 1;
+            this.lblConnectionId.Text = "Connection Id : ";
+            // 
+            // lblStatusValue
+            // 
+            this.lblStatusValue.AutoSize = true;
+            this.lblStatusValue.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.lblStatusValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatusValue.ForeColor = System.Drawing.Color.Snow;
+            this.lblStatusValue.Location = new System.Drawing.Point(218, 7);
+            this.lblStatusValue.Name = "lblStatusValue";
+            this.lblStatusValue.Size = new System.Drawing.Size(21, 29);
+            this.lblStatusValue.TabIndex = 2;
+            this.lblStatusValue.Text = "-";
+            // 
+            // lblConnectionIdValue
+            // 
+            this.lblConnectionIdValue.AutoSize = true;
+            this.lblConnectionIdValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConnectionIdValue.Location = new System.Drawing.Point(209, 41);
+            this.lblConnectionIdValue.Name = "lblConnectionIdValue";
+            this.lblConnectionIdValue.Size = new System.Drawing.Size(21, 29);
+            this.lblConnectionIdValue.TabIndex = 3;
+            this.lblConnectionIdValue.Text = "-";
+            // 
+            // lblPosition
+            // 
+            this.lblPosition.AutoSize = true;
+            this.lblPosition.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPosition.Location = new System.Drawing.Point(474, 9);
+            this.lblPosition.Name = "lblPosition";
+            this.lblPosition.Size = new System.Drawing.Size(29, 29);
+            this.lblPosition.TabIndex = 6;
+            this.lblPosition.Text = "--";
+            // 
+            // btnStream
+            // 
+            this.btnStream.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnStream.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStream.Location = new System.Drawing.Point(1552, 8);
+            this.btnStream.Name = "btnStream";
+            this.btnStream.Size = new System.Drawing.Size(222, 50);
+            this.btnStream.TabIndex = 10;
+            this.btnStream.Text = "Start";
+            this.btnStream.UseVisualStyleBackColor = false;
+            this.btnStream.Click += new System.EventHandler(this.btnStream_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1773, 713);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPort.Location = new System.Drawing.Point(1170, 12);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(147, 29);
+            this.lblPort.TabIndex = 13;
+            this.lblPort.Text = "COM PORT";
+            // 
+            // cboComPort
+            // 
+            this.cboComPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboComPort.FormattingEnabled = true;
+            this.cboComPort.Location = new System.Drawing.Point(1336, 9);
+            this.cboComPort.Name = "cboComPort";
+            this.cboComPort.Size = new System.Drawing.Size(121, 37);
+            this.cboComPort.TabIndex = 12;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(875, 9);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(289, 52);
+            this.axWindowsMediaPlayer1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1788, 862);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.cboComPort);
+            this.Controls.Add(this.btnStream);
+            this.Controls.Add(this.lblPosition);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblConnectionIdValue);
+            this.Controls.Add(this.lblStatusValue);
+            this.Controls.Add(this.lblConnectionId);
+            this.Controls.Add(this.lblStatus);
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblConnectionId;
+        private System.Windows.Forms.Label lblStatusValue;
+        private System.Windows.Forms.Label lblConnectionIdValue;
+        private System.Windows.Forms.Label lblPosition;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnStream;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.ComboBox cboComPort;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+//using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+using Coding4Fun.Kinect.WinForm;
+using System.IO.Ports;
+using System.Windows.Media;
+using System.Windows;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+
+        long avg = 0;
+
+        int iteration = 0;
+
+        int countFor250 = 0;
+
+        int countFor300 = 0;
+
+        int countFor350 = 0;
+
+        int countFor400 = 0;
+
+        int countFor450 = 0;
+
+        int countFor500 = 0;
+
+        int countFor550 = 0;
+
+        int countFor600 = 0;
+
+        bool iskinectDisconnected = false;
+
+        long dataOut;
+
+        private KinectSensor kSensor;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            string[] ports = SerialPort.GetPortNames();
+            foreach (var item in ports)
+            {
+                cboComPort.Items.Add(item);
+                cboComPort.SelectedIndex = 0;
+
+            }
+            lblConnectionId.Visible = false;
+            lblConnectionIdValue.Visible = false;
+            serialPort1.PortName = cboComPort.Text;
+            serialPort1.BaudRate = 9600;
+            serialPort1.DataBits = 8;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.Parity = Parity.None;
+            serialPort1.Open();
+
+        }
+
+        private void btnStream_Click(object sender, EventArgs e)
+        {
+            if (btnStream.Text == "Start")
+            {
+
+                if (KinectSensor.KinectSensors.Count > 0)
+                {
+                    this.btnStream.Text = "Stop";
+
+                    kSensor = KinectSensor.KinectSensors[0];
+
+                    KinectSensor.KinectSensors.StatusChanged += KinectSensors_StatusChanged;
+                }
+                kSensor.Start();
+
+                this.lblConnectionIdValue.Text = kSensor.DeviceConnectionId;
+
+                kSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+                kSensor.DepthStream.Enable();
+
+                kSensor.AllFramesReady += KSensor_AllFramesReady;
+
+                kSensor.SkeletonStream.Enable();
+
+                kSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+            }
+            else
+            {
+                if (kSensor != null && kSensor.IsRunning && btnStream.Text == "Stop")
+                {
+                    kSensor.Stop();
+
+                    this.btnStream.Text = "Start";
+
+                    this.pictureBox1.Image = null;
+
+                    if (serialPort1.IsOpen)
+                    {
+                        serialPort1.Close();
+                    }
+                }
+            }
+
+        }
+
+        private void KSensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            using (var frame = e.OpenColorImageFrame())
+                if (frame != null)
+                {
+                    if (!iskinectDisconnected)
+                        pictureBox1.Image = CreateBitmapFromSensor(frame);
+                }
+
+            using (var frame = e.OpenSkeletonFrame())
+            {
+                if (frame == null)
+                {
+                    return;
+
+                }
+
+                var skeletons = new Skeleton[frame.SkeletonArrayLength];
+                frame.CopySkeletonDataTo(skeletons);
+
+                var TrackedSkeleton = skeletons.FirstOrDefault(x => x.TrackingState == SkeletonTrackingState.Tracked);
+
+                if (TrackedSkeleton == null)
+                {
+                    axWindowsMediaPlayer1.Ctlcontrols.pause();
+                    serialPort1.WriteLine(Convert.ToString(-500));
+                    iteration = 0;
+                    return;
+                }
+
+
+                var position = TrackedSkeleton.Joints[JointType.HandRight].Position;
+
+                var coordinatemapper = new CoordinateMapper(kSensor);
+
+                var colorpoint = coordinatemapper.MapSkeletonPointToColorPoint(position, ColorImageFormat.InfraredResolution640x480Fps30);
+
+                this.lblPosition.Text = string.Format(" Hand Position X:{0}, Y : {1}", colorpoint.X, colorpoint.Y);
+
+                dataOut = colorpoint.X;
+
+                count++;
+                if (dataOut > 150)
+                {
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+
+                    if (dataOut > 150 )
+                    {
+                        countFor250++;
+                    }
+                    if (dataOut > 250 && dataOut < 300)
+                    {
+                        countFor300++;
+                    }
+
+                    if (dataOut > 300 && dataOut < 350)
+                    {
+                        countFor350++;
+                    }
+                    if (dataOut > 350 && dataOut < 400)
+                    {
+                        countFor400++;
+                    }
+                    if (dataOut > 400 && dataOut < 450)
+                    {
+                        countFor450++;
+                    }
+                    if (dataOut > 450 && dataOut < 500)
+                    {
+                        countFor500++;
+                    }
+                    if (dataOut > 500 && dataOut < 550)
+                    {
+                        countFor550++;
+                    }
+                    if (dataOut > 550)
+                    {
+                        countFor600++;
+                    }
+                    if (count > 50 && iteration == 0 && countFor250 > 1)
+                    {
+                        avg = 300;
+                        serialPort1.WriteLine(Convert.ToString(avg));
+                        count = 0;
+                        iteration = 1;
+                        avg = 0;
+                        countFor300 = 0;
+
+                    }
+                    if (count > 50 && iteration == 1 && countFor300 > 1)
+                    {
+                        avg = 300;
+                        serialPort1.WriteLine(Convert.ToString(avg));
+                        count = 0;
+                        iteration = 2;
+                        avg = 0;
+                        countFor300 = 0;
+
+                    }
+                    if (count > 50 && iteration <= 2 && countFor350 > 5)
+                    {
+                        avg = 350;
+                        serialPort1.WriteLine(Convert.ToString(avg));
+                        count = 0;
+                        iteration = 3;
+                        avg = 0;
+                        countFor350 = 0;
+                    }
+                    if (count > 50 && iteration == 3 && countFor400 > 5)
+                    {
+                        avg = 400;
+                        serialPort1.WriteLine(Convert.ToString(avg));
+                        count = 0;
+                        iteration = 4;
+                        avg = 0;
+                        countFor400 = 0;
+
+                    }
+                    if (count > 50 && iteration == 4 && countFor450 > 5)
+                    {
+                        avg = 450;
+                        serialPort1.WriteLine(Convert.ToString(avg));
+                        count = 0;
+                        iteration = 5;
+                        avg = 0;
+                        countFor450 = 0;
+
+                    }
+                    if (count > 50 && iteration == 5 && countFor500 > 5)
+                    {
+                        avg = 500;
+                        serialPort1.WriteLine(Convert.ToString(avg));
+                        count = 0;
+                        iteration = 6;
+                        avg = 0;
+                        countFor500 = 0;
+
+                    }
+                    if (count > 50 && iteration == 6 && countFor550 > 5)
+                    {
+                        avg = 550;
+                        serialPort1.WriteLine(Convert.ToString(avg));
+                        count = 0;
+                        iteration = 7;
+                        avg = 0;
+                        countFor550 = 0;
+
+                    }
+                    if (countFor600 > 15)
+                    {
+                        avg = 700;
+                        serialPort1.WriteLine(Convert.ToString(avg));
+                        count = 0;
+                        iteration = 0;
+                        avg = 0;
+                        countFor600 = 0;
+
+                    }
+                    dataOut = 0;
+                }
+                else
+                {
+                    axWindowsMediaPlayer1.Ctlcontrols.pause();
+                    serialPort1.WriteLine(Convert.ToString(-500));
+                    iteration = 0;
+                    
+                }
+            }
+        }
+
+        //private void KSensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        //{
+        //    using (var frame = e.OpenColorImageFrame())
+        //    {
+        //        if (frame != null)
+        //        {
+        //            if (!iskinectDisconnected)
+        //                pictureBox1.Image = CreateBitmapFromSensor(frame);
+        //        }
+        //    }
+        //}
+
+        private void KinectSensors_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            this.lblStatusValue.Text = kSensor.Status.ToString();
+        }
+
+        private System.Drawing.Bitmap CreateBitmapFromSensor(ColorImageFrame frame)
+        {
+            var pixelData = new byte[frame.PixelDataLength];
+
+            frame.CopyPixelDataTo(pixelData);
+
+            return pixelData.ToBitmap(frame.Width, frame.Height);
+        }
+
+        private void btnStream_Click_1(object sender, EventArgs e)
+        {
+            if (btnStream.Text == "Start")
+            {
+
+                if (KinectSensor.KinectSensors.Count > 0)
+                {
+                    this.btnStream.Text = "Stop";
+
+                    kSensor = KinectSensor.KinectSensors[0];
+
+                    this.lblStatusValue.Text = "Connected";
+
+                    axWindowsMediaPlayer1.URL = "C:\\Users\\Songs\\Shambhu_Sutaya.mp3";
+                }
+                kSensor.Start();
+                iskinectDisconnected = false;
+
+                this.lblConnectionIdValue.Text = kSensor.DeviceConnectionId;
+
+                kSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+                kSensor.DepthStream.Enable();
+
+                kSensor.AllFramesReady += KSensor_AllFramesReady;
+
+                kSensor.SkeletonStream.Enable();
+
+                kSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+            }
+            else
+            {
+                if (kSensor != null && kSensor.IsRunning && btnStream.Text == "Stop")
+                {
+                    kSensor.Stop();
+
+                    iskinectDisconnected = true;
+
+                    serialPort1.WriteLine(Convert.ToString(-500));
+                    iteration = 0;
+                    this.btnStream.Text = "Start";
+
+                    this.lblStatusValue.Text = "Disconnected";
+
+                }
+            }
+        }
+    }
+}
